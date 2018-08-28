7d23e20584d5a1f173e7fe850863c4481d1c54a5
Ubuntu <swaraj@ubuntu16.zkrglnjvunwevel12ff5zm4kpd.rx.internal.cloudapp.net>
2018-08-28 07:51:33 +0000

    recre

---
+++ recreate.sh
@@ -0,0 +1,17 @@
+
+
+composer participant add -c admin@sm -d '{ "$class": "org.hack.sagarmala.VOA", "pID": "VOA11" }'
+composer participant add -c admin@sm -d '{ "$class": "org.hack.sagarmala.VOA", "pID": "VOA12" }'
+composer participant add -c admin@sm -d '{ "$class": "org.hack.sagarmala.VOA", "pID": "VOA13" }'
+composer participant add -c admin@sm -d '{ "$class": "org.hack.sagarmala.VOA", "pID": "VOA14" }'
+composer participant add -c admin@sm -d '{ "$class": "org.hack.sagarmala.VOA", "pID": "VOA15" }'
+composer participant add -c admin@sm -d '{ "$class": "org.hack.sagarmala.VOA", "pID": "VOA15" }'
+composer participant add -c admin@sm -d '{ "$class": "org.hack.sagarmala.VOA", "pID": "VOA15" }'
+composer participant add -c admin@sm -d '{ "$class": "org.hack.sagarmala.VOA", "pID": "VOA15" }'
+composer participant add -c admin@sm -d '{ "$class": "org.hack.sagarmala.VOA", "pID": "VOA15" }'
+composer participant add -c admin@sm -d '{ "$class": "org.hack.sagarmala.VOA", "pID": "VOA15" }'
+composer participant add -c admin@sm -d '{ "$class": "org.hack.sagarmala.VOA", "pID": "VOA15" }'
+composer participant add -c admin@sm -d '{ "$class": "org.hack.sagarmala.VOA", "pID": "VOA15" }'
+composer participant add -c admin@sm -d '{ "$class": "org.hack.sagarmala.VOA", "pID": "VOA15" }'
+composer participant add -c admin@sm -d '{ "$class": "org.hack.sagarmala.VOA", "pID": "VOA15" }'
+composer participant add -c admin@sm -d '{ "$class": "org.hack.sagarmala.VOA", "pID": "VOA15" }'
