81450e987f368bfdc9780e7289cddaa0cd5dc82d
swaraj <jena.swaraj@gmail.com>
2018-09-03 13:22:08 +0530

    add data exec

---
+++ add_data.sh
@@ -0,0 +1,69 @@
+echo "Creating Vessel J0522"
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ "$class": "org.hack.sagarmala.Vessel", "vesselID": "J0522", "DisplayId": "J0522", "Name": "J0522", "containers": ["resource:org.hack.sagarmala.Container#1191"], "voa": "resource:org.hack.sagarmala.VOA#VOA11", "tasks": [ ]  }' 'http://52.172.133.188:4000/api/org.hack.sagarmala.Vessel'
+
+echo "Creating Container CAIU3300786"
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  "$class": "org.hack.sagarmala.Container",  "contID": "CAIU3300786", "DisplayId": "CAIU3300786",  "transportType": "TRUCK",  "location": "PREARRIVAL",  "designated": "DPD",  "transportID": "string",  "owner": "resource:org.hack.sagarmala.Cuctomer#3434",  "vesselID": "J0522",  "tasks": [  ]  }' 'http://52.172.133.188:4000/api/org.hack.sagarmala.Container'
+
+echo "Creating Container CRSU1118036"
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  "$class": "org.hack.sagarmala.Container",  "contID": "CRSU1118036", "DisplayId": "CRSU1118036",  "transportType": "TRUCK",  "location": "PREARRIVAL",  "designated": "DPD",  "transportID": "string",  "owner": "resource:org.hack.sagarmala.Cuctomer#3434",  "vesselID": "J0522",  "tasks": [  ]  }' 'http://52.172.133.188:4000/api/org.hack.sagarmala.Container'
+
+echo "Creating Container CXRU1333001"
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  "$class": "org.hack.sagarmala.Container",  "contID": "CXRU1333001", "DisplayId": "CXRU1333001",  "transportType": "TRUCK",  "location": "PREARRIVAL",  "designated": "DPD",  "transportID": "string",  "owner": "resource:org.hack.sagarmala.Cuctomer#3434",  "vesselID": "J0522",  "tasks": [  ]  }' 'http://52.172.133.188:4000/api/org.hack.sagarmala.Container'
+
+echo "Creating Container DVRU1573463"
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  "$class": "org.hack.sagarmala.Container",  "contID": "DVRU1573463", "DisplayId": "DVRU1573463",  "transportType": "TRUCK",  "location": "PREARRIVAL",  "designated": "DPD",  "transportID": "string",  "owner": "resource:org.hack.sagarmala.Cuctomer#3434",  "vesselID": "J0522",  "tasks": [  ]  }' 'http://52.172.133.188:4000/api/org.hack.sagarmala.Container'
+
+echo "Creating Container RLTU2006336"
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  "$class": "org.hack.sagarmala.Container",  "contID": "RLTU2006336", "DisplayId": "RLTU2006336",  "transportType": "TRUCK",  "location": "PREARRIVAL",  "designated": "DPD",  "transportID": "string",  "owner": "resource:org.hack.sagarmala.Cuctomer#3434",  "vesselID": "J0522",  "tasks": [  ]  }' 'http://52.172.133.188:4000/api/org.hack.sagarmala.Container'
+
+
+
+echo "Creating Vessel J0551"
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ "$class": "org.hack.sagarmala.Vessel", "vesselID": "J0551", "DisplayId": "J0551", "Name": "J0551", "containers": ["resource:org.hack.sagarmala.Container#114491"], "voa": "resource:org.hack.sagarmala.VOA#VOA12", "tasks": [ ]  }' 'http://52.172.133.188:4000/api/org.hack.sagarmala.Vessel'
+
+echo "Creating Container RWAU2400936"
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  "$class": "org.hack.sagarmala.Container",  "contID": "RWAU2400936", "DisplayId": "RWAU2400936",  "transportType": "TRUCK",  "location": "PREARRIVAL",  "designated": "DPD",  "transportID": "string",  "owner": "resource:org.hack.sagarmala.Cuctomer#3434",  "vesselID": "J0551",  "tasks": [  ]  }' 'http://52.172.133.188:4000/api/org.hack.sagarmala.Container'
+
+echo "Creating Container TCKU3391234"
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  "$class": "org.hack.sagarmala.Container",  "contID": "TCKU3391234", "DisplayId": "TCKU3391234",  "transportType": "TRUCK",  "location": "PREARRIVAL",  "designated": "DPD",  "transportID": "string",  "owner": "resource:org.hack.sagarmala.Cuctomer#3434",  "vesselID": "J0551",  "tasks": [  ]  }' 'http://52.172.133.188:4000/api/org.hack.sagarmala.Container'
+
+echo "Creating Container TCLU9642416"
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  "$class": "org.hack.sagarmala.Container",  "contID": "TCLU9642416", "DisplayId": "TCLU9642416",  "transportType": "TRUCK",  "location": "PREARRIVAL",  "designated": "DPD",  "transportID": "string",  "owner": "resource:org.hack.sagarmala.Cuctomer#3434",  "vesselID": "J0551",  "tasks": [  ]  }' 'http://52.172.133.188:4000/api/org.hack.sagarmala.Container'
+
+echo "Creating Container TGHU0937415"
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  "$class": "org.hack.sagarmala.Container",  "contID": "TGHU0937415", "DisplayId": "TGHU0937415",  "transportType": "TRUCK",  "location": "PREARRIVAL",  "designated": "DPD",  "transportID": "string",  "owner": "resource:org.hack.sagarmala.Cuctomer#3434",  "vesselID": "J0551",  "tasks": [  ]  }' 'http://52.172.133.188:4000/api/org.hack.sagarmala.Container'
+
+echo "Creating Container TRHU2445057"
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  "$class": "org.hack.sagarmala.Container",  "contID": "TRHU2445057", "DisplayId": "TRHU2445057",  "transportType": "TRUCK",  "location": "PREARRIVAL",  "designated": "DPD",  "transportID": "string",  "owner": "resource:org.hack.sagarmala.Cuctomer#3434",  "vesselID": "J0551",  "tasks": [  ]  }' 'http://52.172.133.188:4000/api/org.hack.sagarmala.Container'
+
+
+echo "Creating Vessel J0568"
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ "$class": "org.hack.sagarmala.Vessel", "vesselID": "J0568", "DisplayId": "J0568", "Name": "J0568", "containers": ["resource:org.hack.sagarmala.Container#1144391"], "voa": "resource:org.hack.sagarmala.VOA#VOA13", "tasks": [ ]  }' 'http://52.172.133.188:4000/api/org.hack.sagarmala.Vessel'
+
+echo "Creating Container FCIU4788436"
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  "$class": "org.hack.sagarmala.Container",  "contID": "FCIU4788436", "DisplayId": "FCIU4788436",  "transportType": "TRUCK",  "location": "PREARRIVAL",  "designated": "DPD",  "transportID": "string",  "owner": "resource:org.hack.sagarmala.Cuctomer#3434",  "vesselID": "J0568",  "tasks": [  ]  }' 'http://52.172.133.188:4000/api/org.hack.sagarmala.Container'
+
+echo "Creating Container KKTU7917885"
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  "$class": "org.hack.sagarmala.Container",  "contID": "KKTU7917885", "DisplayId": "KKTU7917885",  "transportType": "TRUCK",  "location": "PREARRIVAL",  "designated": "DPD",  "transportID": "string",  "owner": "resource:org.hack.sagarmala.Cuctomer#3434",  "vesselID": "J0568",  "tasks": [  ]  }' 'http://52.172.133.188:4000/api/org.hack.sagarmala.Container'
+
+
+
+echo "Creating Vessel J0604"
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ "$class": "org.hack.sagarmala.Vessel", "vesselID": "J0604", "DisplayId": "J0604", "Name": "J0604", "containers": ["resource:org.hack.sagarmala.Container#114444291"], "voa": "resource:org.hack.sagarmala.VOA#VOA14", "tasks": [ ]  }' 'http://52.172.133.188:4000/api/org.hack.sagarmala.Vessel'
+
+echo "Creating Container KKTU8255741"
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  "$class": "org.hack.sagarmala.Container",  "contID": "KKTU8255741", "DisplayId": "KKTU8255741",  "transportType": "TRUCK",  "location": "PREARRIVAL",  "designated": "DPD",  "transportID": "string",  "owner": "resource:org.hack.sagarmala.Cuctomer#3434",  "vesselID": "J0604",  "tasks": [  ]  }' 'http://52.172.133.188:4000/api/org.hack.sagarmala.Container'
+
+echo "Creating Container MOAU0726347"
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  "$class": "org.hack.sagarmala.Container",  "contID": "MOAU0726347", "DisplayId": "MOAU0726347",  "transportType": "TRUCK",  "location": "PREARRIVAL",  "designated": "DPD",  "transportID": "string",  "owner": "resource:org.hack.sagarmala.Cuctomer#3434",  "vesselID": "J0604",  "tasks": [  ]  }' 'http://52.172.133.188:4000/api/org.hack.sagarmala.Container'
+
+
+
+echo "Creating Vessel J0662"
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ "$class": "org.hack.sagarmala.Vessel", "vesselID": "J0662", "DisplayId": "J0662", "Name": "J0662", "containers": ["resource:org.hack.sagarmala.Container#114494441"], "voa": "resource:org.hack.sagarmala.VOA#VOA15", "tasks": [ ]  }' 'http://52.172.133.188:4000/api/org.hack.sagarmala.Vessel'
+
+echo "Creating Container MOAU1426494"
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  "$class": "org.hack.sagarmala.Container",  "contID": "MOAU1426494", "DisplayId": "MOAU1426494",  "transportType": "TRUCK",  "location": "PREARRIVAL",  "designated": "DPD",  "transportID": "string",  "owner": "resource:org.hack.sagarmala.Cuctomer#3434",  "vesselID": "J0662",  "tasks": [  ]  }' 'http://52.172.133.188:4000/api/org.hack.sagarmala.Container'
+
+echo "Creating Container KOUF1234564"
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  "$class": "org.hack.sagarmala.Container",  "contID": "KOUF1234564", "DisplayId": "KOUF1234564",  "transportType": "TRUCK",  "location": "PREARRIVAL",  "designated": "DPD",  "transportID": "string",  "owner": "resource:org.hack.sagarmala.Cuctomer#3434",  "vesselID": "J0662",  "tasks": [  ]  }' 'http://52.172.133.188:4000/api/org.hack.sagarmala.Container'
